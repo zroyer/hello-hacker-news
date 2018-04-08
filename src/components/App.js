7c21a8e29fc4a6858d58fcd2417b0d3c81e4fcbe
Zach Royer <zro@crowdnoise.com>
2018-04-08 14:35:04 -0500

    adds some components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,10 @@
-import React, { Component } from 'react';
-import '../style/App.css';
+import React, { Component } from 'react'
+import LinkList from './LinkList'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <LinkList />
   }
 }
 
-export default App;
+export default App
