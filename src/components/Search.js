3be461d1cc7960eab74084233cd57f65cd848509
Zach Royer <zro@crowdnoise.com>
2018-04-08 23:00:32 -0500

    adds feed search query

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,6 +29,39 @@ class Search extends Component {
       </div>
     )
   }
+
+  _executeSearch = async () => {
+    const { filter } = this.state
+    const result = await this.props.client.query({
+      query: FEED_SEARCH_QUERY,
+      variables: { filter },
+    })
+    const links = result.data.feed.links
+    this.setState({ links })
+  }
 }
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
 
 export default withApollo(Search)
