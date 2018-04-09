0805c413566af9746c3782379fcdb55a5ddd41a4
Zach Royer <zro@crowdnoise.com>
2018-04-08 23:22:06 -0500

    adds subscriptions

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -5,6 +5,11 @@ import gql from 'graphql-tag'
 import Link from './Link'
 
 class LinkList extends Component {
+  componentDidMount() {
+    this._subscribeToNewLinks()
+    this._subscribeToNewVotes()
+  }
+
   render() {
     if (this.props.feedQuery && this.props.feedQuery.loading) {
       return <div>Loading</div>
@@ -32,6 +37,76 @@ class LinkList extends Component {
     votedLink.votes = createVote.link.votes
 
     store.writeQuery({ query: FEED_QUERY, data })
+  }
+
+  _subscribeToNewLinks = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newLink {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [subscriptionData.data.newLink.node, ...previous.feed.links]
+        const result = {
+          ...previous,
+          feed: {
+            links: newAllLinks
+          },
+        }
+        return result
+      },
+    })
+  }
+
+  _subscribeToNewVotes = () => {
+    this.props.feedQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          newVote {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloClient } from 'apollo-client'
-import { ApolloLink } from 'apollo-client-preset'
 import { ApolloProvider } from 'react-apollo'
+import { ApolloLink, split } from 'apollo-client-preset'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { AUTH_TOKEN } from './constants'
@@ -13,6 +15,30 @@ import './style/index.css'
 import App from './components/App'
 
 const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem(AUTH_TOKEN)
